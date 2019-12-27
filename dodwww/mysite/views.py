64ff256281ba4f863a6f3025c1ec4eb8b6a5fe8c
Richard Ho <skynet.tw@gmail.com>
2019-12-27 08:06:59 +0800

    correct the json output format

--- dodwww/mysite/views.py
+++ dodwww/mysite/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from mysite.models import Store, Drinks
+import json
 
 def index(request):
 	
 	return render(request, "index.html")
 
 def query(request, qstr="none"):
-    res = {'qstr':qstr}
-    return JsonResponse(res)
+    
+    if qstr == 'allstores':
+        allstores = Store.objects.all()
+        res = list(allstores.values())
+    else:
+        res = {'key':'none'}
+
+    return JsonResponse(res, safe=False)
