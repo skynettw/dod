ec61f5f2d86769ac6e9cb23f42434166d38770f0
Richard Ho <skynet.tw@gmail.com>
2019-12-30 13:47:53 +0800

    fix admin.py

--- dodwww/mysite/admin.py
+++ dodwww/mysite/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from mysite.models import Drinks, Store
 
 class DrinksAdmin(admin.ModelAdmin):
-    list_display = ('store', 'name', 'price', 'cup', 'calorie')
+    list_display = ('store', 'name', 'price', 'cup', 'calorie', 'milk', 'tea', 'cafe', 'juice', 'bubble', 'hotcold')
 
 admin.site.register(Drinks, DrinksAdmin)
 admin.site.register(Store)
